0ed6ddfa5d734831c27ab3601c548ed07b039156
Kantapong Klinnaim <116998478+Kantapong11755@users.noreply.github.com>
2023-12-12 10:11:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
+app.get('/product-forklifts', (req, res) => {
+    db.query("SELECT * FROM forklifts", (err, result) => {
+        if (err) {
+            console.log(err);
+        }else {
+            res.send(result);
+        }
+    })
+})
+
+
+app.get('/product-seller', (req, res) => {
+    db.query('SELECT * FROM product_gp', (err, result) => {
+        if (err) {
+            console.log(err);
+        }else {
+            res.send(result);
+        }
+    })
+})
+
+
+app.get('/product-all/:model', async (req, res) => {
+    const model = req.params.model;
+    console.log(model);
+    db.query('SELECT * FROM product WHERE MODEL = ?', [model], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+
+
+app.listen('3000', () => {
+    console.log('Server is running on port 3000');
+})
